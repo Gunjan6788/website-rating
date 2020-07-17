deaec0253bead163271cb7ef98781f654680de5b
Aman Kumar <amankumar4real@gmail.com>
2020-07-18 04:43:34 +0530

    updated ui

--- popup.js
+++ popup.js
@@ -1,23 +1,4 @@
 
-//data of the stars
-
-let star = document.querySelectorAll('i')
-console.log(star)
-for(let i=0; i<star.length; i++){
-    star[i].addEventListener('click', function(){
-        console.log(i+1)
-    })
-}
-
-
-
-// fetching name and review by the user
-
-let name = document.getElementById('name').value
-let review = document.getElementById('review').value
-
-
-
 // getting data of current domain
 
 var domain = ""
@@ -32,9 +13,28 @@ chrome.tabs.query({active: true, currentWindow: true}, tabs => {
 
 let btn_review = document.getElementById('btn_review')
 
+
+//data of the stars
+
+var star = document.querySelectorAll('i')
+
+var stars_given = 0
+
+for(let i=0; i<star.length; i++){
+    star[i].addEventListener('click', function(){
+        stars_given = i+1
+        alert(i+1)
+    })
+}
+
 btn_review.addEventListener('click', function(){
     event.preventDefault()
+
+    // fetching name and review by the user
+
+    let name = document.getElementById('name').value
+    let review = document.getElementById('review').value
+
     
-    // set and get
     
 })
